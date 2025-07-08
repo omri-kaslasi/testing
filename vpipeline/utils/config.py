d238785e01f290218a2e99c4291896bd0636e53f
Omri Kaslasi <omri.k@sygnia.co>
2025-07-08 20:25:15 +0300

    wip vector manager

--- vpipeline/utils/config.py
+++ vpipeline/utils/config.py
@@ -4,24 +4,24 @@ from pathlib import Path
 
 
 def get_config_value(key, default=None):
-    """Get a configuration value from the environment variables."""
+    """Get a configuration value from the environment variables.
+    If the variable exists (even if empty), returns its value;
+    otherwise returns the provided default.
+    """
+    if key in os.environ:
+        return os.environ[key]
     return default
-    #return os.getenv(key, default)
-
 
 # file_loader
 
 
-
 # vector_config_manager
-
 CONF_DIR = get_config_value("VPIPELINE_CONF_DIR",Path('/mnt/vector-configs'))
 FILTER_OUT_PORT = get_config_value("VPIPELINE_FILTER_OUT_PORT",9001)
 FILTER_IN_PORT = get_config_value("VPIPELINE_FILTER_IN_PORT",9000)
 
 
 # initiative_manager
-
 DATA_DIR = get_config_value("DATA_DIR",Path('data/initiatives'))
 
 # sink_config_builder
